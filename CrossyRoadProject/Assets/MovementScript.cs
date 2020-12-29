ed850458765cb2b7cff037ace4a45f352fb9f4f1
SebMatousek <seb.matousek@gmail.com>
2020-12-29 16:07:24 +0100

    Chicken movement begun, now reacts to 'W'

---
+++ CrossyRoadProject/Assets/MovementScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    [SerializeField] private float speed = 0.25f;
+    [SerializeField] public Rigidbody rb;
+    [SerializeField] public float jumpForce = 2.1f;
+
+    public Vector3 jump;
+    public bool isGrounded;
+
+    void Start()
+    {
+        jump = new Vector3(0.0f, jumpForce, speed);
+    }
+
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.W) && isGrounded)
+        {
+            rb.AddForce(jump, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    void OnCollisionStay()
+    {
+        isGrounded = true;
+
+    }
+
+    void OnCollisionExit()
+    {
+        isGrounded = false;
+    }
+}
