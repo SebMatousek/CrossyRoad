2d3413bbfae45167a5063e7ddbcfb6118c95a938
Sashking <sashking.cz@gmail.com>
2021-01-02 14:10:36 +0100

    Row generation + camera following only on X axis

---
+++ CrossyRoadProject/Assets/FollowObjectScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowObjectScript : MonoBehaviour
+{
+    [SerializeField] private Transform objectToFollow;
+
+    private void Update()
+    {
+        transform.position = new Vector3(objectToFollow.position.x, 0, 0);
+    }
+}
