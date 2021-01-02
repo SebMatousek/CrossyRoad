c3cff80fda797dbd509c671f377164738cfb6b01
Sashking <sashking.cz@gmail.com>
2021-01-02 16:56:26 +0100

    dont pull this one either

--- CrossyRoadProject/Assets/MovementScript.cs
+++ CrossyRoadProject/Assets/MovementScript.cs
@@ -4,14 +4,6 @@ using UnityEngine;
 
 public class MovementScript : MonoBehaviour
 {
-<<<<<<< HEAD:CrossyRoadProject/Assets/Scripts/MovementScript.cs
-    public bool isGrounded;
-
-    [Range(1, 10)]
-    public float jumpVelocity;
-    [Range(1, 10)]
-    public float playerVelocity;
-=======
     [SerializeField] private float speed = 0.25f;
     [SerializeField] public Rigidbody rb;
     [SerializeField] public float jumpForce = 2.1f;
@@ -24,30 +16,13 @@ public class MovementScript : MonoBehaviour
         jump = new Vector3(0.0f, jumpForce, speed);
     }
 
->>>>>>> parent of 2d3413b... Row generation + camera following only on X axis:CrossyRoadProject/Assets/MovementScript.cs
 
-    private void Update()
+    private void FixedUpdate()
     {
-<<<<<<< HEAD:CrossyRoadProject/Assets/Scripts/MovementScript.cs
-        if (isGrounded)
-=======
         if (Input.GetKeyDown(KeyCode.W) && isGrounded)
->>>>>>> parent of 2d3413b... Row generation + camera following only on X axis:CrossyRoadProject/Assets/MovementScript.cs
         {
-            if (Input.GetKeyDown(KeyCode.W))
-            {
-                GetComponent<Rigidbody>().velocity = new Vector3(0, jumpVelocity, playerVelocity);
-            }
-
-            if (Input.GetKeyDown(KeyCode.A))
-            {
-                GetComponent<Rigidbody>().velocity = new Vector3(-playerVelocity, jumpVelocity, 0);
-            }
-
-            if (Input.GetKeyDown(KeyCode.D))
-            {
-                GetComponent<Rigidbody>().velocity = new Vector3(playerVelocity, jumpVelocity, 0);
-            }
+            rb.AddForce(jump, ForceMode.Impulse);
+            isGrounded = false;
         }
     }
 
