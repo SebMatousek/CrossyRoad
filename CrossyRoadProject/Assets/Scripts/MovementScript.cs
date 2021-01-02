6840a531165dbb28f77af0d9b36345db41d06daa
Sashking <sashking.cz@gmail.com>
2021-01-02 17:13:21 +0100

    Row generation + camera following only on X axis

--- CrossyRoadProject/Assets/Scripts/MovementScript.cs
+++ CrossyRoadProject/Assets/Scripts/MovementScript.cs
@@ -5,34 +5,35 @@ using UnityEngine;
 public class MovementScript : MonoBehaviour
 {
     [SerializeField] private float speed = 0.25f;
-    [SerializeField] public Rigidbody rb;
+    private Rigidbody rb;
     [SerializeField] public float jumpForce = 2.1f;
 
     public Vector3 jump;
     public bool isGrounded;
 
-    void Start()
+    private void Start()
     {
-        jump = new Vector3(0.0f, jumpForce, speed);
+        rb = GetComponent<Rigidbody>();
+        jump = new Vector3(speed, jumpForce, 0);
     }
 
 
     private void FixedUpdate()
     {
-        if (Input.GetKeyDown(KeyCode.W) && isGrounded)
+        if (Input.GetKey(KeyCode.W) && isGrounded)
         {
             rb.AddForce(jump, ForceMode.Impulse);
             isGrounded = false;
         }
     }
 
-    void OnCollisionStay()
+    private void OnCollisionStay()
     {
         isGrounded = true;
 
     }
 
-    void OnCollisionExit()
+    private void OnCollisionExit()
     {
         isGrounded = false;
     }
